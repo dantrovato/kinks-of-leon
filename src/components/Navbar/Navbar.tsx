f468556529b69bbf14ea651978cd97cef8efcea2
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-11-15 16:41:54 +0000

    Add toggle button functionality for mobiles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,21 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Navbar = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-black">
       <div className="container-fluid m-3">
         <span className="navbar-brand text-white">Kinks of Leon</span>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+        <button className="navbar-toggler" type="button" onClick={toggleNavbar}>
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarText">
+        <div className={`collapse navbar-collapse ${collapsed ? "" : "show"}`}>
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item active">
               <a className="nav-link text-white" href="#">
