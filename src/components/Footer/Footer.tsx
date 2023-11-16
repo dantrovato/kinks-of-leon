e889347956eef6f6331195dddb2deb9ad4a90df0
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-11-16 21:23:21 +0000

    Make background of #root div black

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="container">
+    <div className="container text-white bg-black">
       <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
         <div className="col mb-3">
           <a
@@ -9,7 +9,7 @@ const Footer = () => {
           >
             <svg className="bi me-2" width="40" height="32"></svg>
           </a>
-          <p className="text-body-secondary">© 2023</p>
+          <p className="">© 2023</p>
         </div>
 
         <div className="col mb-3"></div>
@@ -18,15 +18,15 @@ const Footer = () => {
           <h5>Why a tour?</h5>
           <p>
             You can find plenty of things to do by yourself in London but the
-            London kinky scene can be cliquey. If you want to be part of a
-            group, we are at the epicenter of the scene and will be able to
-            introduce you the inner crowd, as well as giving you introductions
-            to the best after parties{" "}
+            kinky scene can be cliquey. If you want to be part of a group
+            without having to find your crew yourself, we are at the epicenter
+            of the scene and will put people together for you{" "}
           </p>
         </div>
         <div className="col mb-3">
           <h5>Contact</h5>
-          <p>Leon is a silly willy</p>
+          <p>Leon +44 7774 314421</p>
+          <p>Dan +44 7984 290884</p>
         </div>
         <div className="col mb-3">
           <h5>Bespoke tours</h5>
